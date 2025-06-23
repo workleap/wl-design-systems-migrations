76a33fabc18f0c33be31b1024e4036b98dccf1eb
Alexandre Asselin <alexandre.asselin@gsoft.com>
2025-06-23 16:07:31 -0400

    update eslint

--- src/shims/Transition.tsx
+++ src/shims/Transition.tsx
@@ -4,27 +4,27 @@ import {
   mergeProps,
   type DivProps
 } from "@hopper-ui/components";
-import { forwardRef, RefObject, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { forwardRef, useCallback, useEffect, useMemo, useRef, useState, type RefObject } from "react";
 
 import "./Transition.css";
 
 export interface TransitionProps extends DivProps {
-    /**
+  /**
      * 	Whether the transition should run on initial mount.
      */
-    animateFirstRender?: boolean;
-    /**
+  animateFirstRender?: boolean;
+  /**
      * CSS classes to add to the transitioning element during the enter phase.
      */
-    enter?: string;
-    /**
+  enter?: string;
+  /**
      * CSS classes to add to the transitioning element during the leave phase.
      */
-    leave?: string;
-    /**
+  leave?: string;
+  /**
      * A controlled show value that determined whether or not the component is displayed.
      */
-    show: boolean;
+  show: boolean;
 }
 
 /**
@@ -33,73 +33,73 @@ export interface TransitionProps extends DivProps {
  * [Documentation](https://wl-orbiter-website.netlify.app/?path=/docs/transition--page)
 */
 export const Transition = forwardRef<HTMLDivElement, TransitionProps >(({
-    animateFirstRender = false,
-    children,
-    enter,
-    leave,
-    show,
-    ...rest
-}, ref) =>  {
-    const [isVisible, setIsVisible] = useState(show);
+  animateFirstRender = false,
+  children,
+  enter,
+  leave,
+  show,
+  ...rest
+}, ref) => {
+  const [isVisible, setIsVisible] = useState(show);
 
-    const isInitialRender = useCommittedRef(useIsInitialRender());
+  const isInitialRender = useCommittedRef(useIsInitialRender());
 
-    useEffect(() => {
-        if (show) {
-            setIsVisible(true);
-        } else if (isNilOrEmpty(leave)) {
-            setIsVisible(false);
-        }
-    }, [show, leave]);
-
-    const handleAnimationEnd = useCallback(() => {
-        setIsVisible(show);
-    }, [show]);
-
-    const className = useMemo(() => {
-        return show
-            ? isInitialRender.current
-                ? animateFirstRender ? enter : undefined
-                : enter
-            : leave;
-    }, [isInitialRender, animateFirstRender, enter, leave, show]);
-
-    if (!isVisible) {
-        return null;
+  useEffect(() => {
+    if (show) {
+      setIsVisible(true);
+    } else if (isNilOrEmpty(leave)) {
+      setIsVisible(false);
     }
+  }, [show, leave]);
 
-    return (
-        <Div
-            {...mergeProps(
-                rest,
-                {
-                    className,
-                    onAnimationEnd: handleAnimationEnd,
-                    ref: ref
-                }
-            )}
-        >
-            {children}
-        </Div>
-    );
-})
+  const handleAnimationEnd = useCallback(() => {
+    setIsVisible(show);
+  }, [show]);
+
+  const className = useMemo(() => {
+    return show
+      ? isInitialRender.current
+        ? animateFirstRender ? enter : undefined
+        : enter
+      : leave;
+  }, [isInitialRender, animateFirstRender, enter, leave, show]);
+
+  if (!isVisible) {
+    return null;
+  }
+
+  return (
+    <Div
+      {...mergeProps(
+        rest,
+        {
+          className,
+          onAnimationEnd: handleAnimationEnd,
+          ref: ref
+        }
+      )}
+    >
+      {children}
+    </Div>
+  );
+});
 
 // Copied from https://github.com/react-restart/hooks/blob/master/src/useCommittedRef.ts.
 function useCommittedRef<T>(value: T): RefObject<T> {
-    const ref = useRef(value);
+  const ref = useRef(value);
 
-    useEffect(() => {
-        ref.current = value;
-    }, [value]);
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
 
-    return ref;
+  return ref;
 }
 
 function useIsInitialRender() {
   const [isInitialRef, setIsInitial] = useRefState(true);
 
   useEffect(() => {
-      setIsInitial(false);
+    setIsInitial(false);
   }, [setIsInitial]);
 
   return isInitialRef.current;
@@ -111,13 +111,13 @@ function useRefState<T>(initialValue: T): [RefObject<T>, (newValue: T, rerender?
   const valueRef = useRef<T>(initialValue);
 
   const setValue = useCallback((newValue: T, rerender = false) => {
-      if (valueRef.current !== newValue) {
-          valueRef.current = newValue;
+    if (valueRef.current !== newValue) {
+      valueRef.current = newValue;
 
-          if (rerender) {
-              forceRender();
-          }
+      if (rerender) {
+        forceRender();
       }
+    }
   }, [valueRef, forceRender]);
 
   return [valueRef, setValue];
@@ -127,6 +127,6 @@ function useForceRender() {
   const [, set] = useState(false);
 
   return useCallback(() => {
-      set(x => !x);
+    set(x => !x);
   }, [set]);
 }
